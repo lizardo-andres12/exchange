16172d2d545002d70e0e52c91a5c1aae9f0f5858
lizardo-andres12 <lizardo.andres12@gmail.com>
2026-05-25 17:55:52 -0400

    Update order book api to declare private helper methods

--- include/containers/order_book.h
+++ include/containers/order_book.h
@@ -2,6 +2,8 @@
 /// instrument traded on this exchange.
 #pragma once
 
+#include <array>
+#include <iostream>
 #include <vector>
 #include <deque>
 #include <unordered_map>
@@ -9,11 +11,25 @@
 #include "common.h"
 
 /// @brief Encapsulates all custom container logic.
-namespace containers {
+namespace containers
+{
+    /// @brief The bad alloc error bit. Use this bit to indicate an instance
+    /// of std::bad_alloc was thrown when using a container.
+    static constexpr size_t BAD_ALLOC_ERROR = 8;
+    /// @brief The undefined enum error bit. Use this bit to indicate a value
+    /// was passed as an enum that does not actually belong to the enum.
+    static constexpr size_t UNDEFINED_ENUM_ERROR = 9;
+
     /// @brief An order container that behaves like the classic central order
     /// book used by many exchanges.
-    class order_book {
+    class order_book
+    {
     public:
+        /// @brief Type alias for the price-specific order queue.
+        using PriceLevelQueue = std::deque<Order>;
+        /// @brief Type alias for the side-specific price level storage structure.
+        using PriceLevelsStructure = std::vector<std::pair<Price, PriceLevelQueue>>;
+
         /// @brief Default order book constructor. Creates an order book with
         /// DEFAULT_RESERVE_COUNT many price levels reserved on both sides.
         explicit order_book() : order_book(DEFAULT_RESERVE_COUNT) {}
@@ -23,7 +39,11 @@ namespace containers {
         /// @param reserve_count The number of price levels to reserve on both
         /// bid and ask sides.
         explicit order_book(const size_t reserve_count)
-            : bids_(reserve_count), asks_(reserve_count) {}
+            : price_levels_()
+        {
+            price_levels_[static_cast<size_t>(Side::Bid)].reserve(reserve_count);
+            price_levels_[static_cast<size_t>(Side::Ask)].reserve(reserve_count);
+        }
 
         /// @brief Inserts a new order in the corresponding side of the order book.
         /// This method may allocate memory if the price level is out of the reserved
@@ -50,20 +70,82 @@ namespace containers {
         order_book &operator=(const order_book &other) = delete;
         order_book &operator=(order_book &&other) = delete;
 
+        void debug() const noexcept
+        {
+            for (const auto &pls : price_levels_)
+            {
+                for (const auto &[price, plq] : pls)
+                {
+                    std::cout << price << ' ';
+                    for (const auto &o : plq)
+                    {
+                        std::cout << o.qty << ' ';
+                    }
+                    std::cout << '\n';
+                }
+            }
+
+            for (const auto &[id, order] : id_order_map_)
+            {
+                std::cout << id << ", " << order->price << '\n';
+            }
+        }
+
     private:
-        /// @brief Vector of price to price level stores sorted by price in ascending order (worst bid is
-        /// at index 0, best bid is at last index). Use rbegin() as the best bid iterator.
-        std::vector<std::pair<Price, std::deque<Order>>> bids_;
-
-        /// @brief Vector of price to price level stores sorted by price in descending order (worst ask is
-        /// at index 0, best ask is at last index). Use rbegin() as the best ask iterator.
-        std::vector<std::pair<Price, std::deque<Order>>> asks_;
-
         /// @brief Maps order ID to Order metadata. This will be the main store
         /// for order status.
-        std::unordered_map<ID, std::deque<Order>::iterator> id_order_map_;
+        std::unordered_map<ID, PriceLevelQueue::iterator> id_order_map_;
+
+        /// @brief Array of price level structures that can be indexed by side enum. Array indices contain
+        /// a vector of price to price level stores sorted by price in ascending/descending order (worst bid/ask is
+        /// at index 0, best bid/ask is at last index). Use rbegin() as the best bid/ask iterator.
+        std::array<PriceLevelsStructure, 2> price_levels_;
 
         /// @brief The default number of price levels to reserve.
         static constexpr size_t DEFAULT_RESERVE_COUNT = 50uz;
+
+        /// @brief Private helper method that returns a reverse iterator
+        /// to the price level queue for the price or an iterator that is one
+        /// past the correct location for the price level queue.
+        ///
+        /// This method is marked noexcept because all exceptions should be caught
+        /// inside the function and should set the appropriate error bits.
+        /// @param error The status bitset used to indicate an error occured.
+        /// @param price The price of the price level to find.
+        /// @param pl the side-specific price level structure (bid side or ask side).
+        /// @return A reverse iterator pointing to the correct location of the price
+        /// level.
+        PriceLevelsStructure::reverse_iterator
+        __find_price_level_disptacher(ErrStatus &error, const Price price,
+                                      PriceLevelsStructure &pl, const Side side) const noexcept;
+
+        /// @brief Private helper for searching bid price level store.
+        /// @param error The status bitset used to indicate an error occured.
+        /// @param price The price of the price level to find.
+        /// @param pl the side-specific price level structure (bid side or ask side).
+        /// @return A reverse iterator pointing to the correct location of the price
+        /// level.
+        PriceLevelsStructure::reverse_iterator
+        __find_price_level_bid(ErrStatus &error, const Price price,
+                               PriceLevelsStructure &pl) const noexcept;
+
+        /// @brief Private helper for searching ask price level store.
+        /// @param error The status bitset used to indicate an error occured.
+        /// @param price The price of the price level to find.
+        /// @param pl the side-specific price level structure (bid side or ask side).
+        /// @return A reverse iterator pointing to the correct location of the price
+        /// level.
+        PriceLevelsStructure::reverse_iterator
+        __find_price_level_ask(ErrStatus &error, const Price price,
+                               PriceLevelsStructure &pl) const noexcept;
+
+        /// @brief Private helper method to add an order to the price level
+        /// queue for the correct price based on order.
+        /// @param error The status bitset used to indicate an error occured.
+        /// @param plq The price level queue `order` should be added to.
+        /// @param order The order to add to `plq`.
+        void __add_order(ErrStatus &error,
+                         PriceLevelQueue &plq, const Order &order) noexcept;
     };
+
 };
